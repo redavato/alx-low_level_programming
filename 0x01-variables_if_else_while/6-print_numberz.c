c2fefea538126456a289479d002476794c9edade
reda achargui <redavato7@gmail.com>
2023-03-17 14:59:57 -0700

    numBers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry number
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
+	putchar(d);
+	putchar('\n');
+
+	return (0);
+}
