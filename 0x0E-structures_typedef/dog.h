691752a951013100f4f398a95f29b441c0255091
reda achargui <redavato7@gmail.com>
2023-04-17 16:57:04 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,5 @@
+#ifndef DOG_H
+#define DOG_H
 /**
  * struct dog - a dog's basic info
  * @name: First member
@@ -24,3 +26,4 @@ dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
+#endif
